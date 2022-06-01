6df7cdc95afdb60995dd8fe0d0156097dfbb5d1d
santiago1525 <s4nt14g03015@gmail.com>
2022-05-31 20:45:15 -0500

    Se sube el proyecto

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,13 @@
+import '../hojasDeEstilo/Boton.css';
+
+function Boton({ texto, esBotonDeClic, manejarClic }){
+    return(
+        <button
+            className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+            onClick={manejarClic}>
+            {texto}
+        </button>
+    );
+}
+
+export default Boton;
